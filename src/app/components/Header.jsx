ddc535f79e63cd29be92f6fe0220e3f84bf81bf2
Kashmala <kashmalaaslam1058@gmail.com>
2024-08-13 11:54:59 +0500

    first commit

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,68 +1,173 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { IoLogoGithub } from "react-icons/io";
-import { FiMoon } from "react-icons/fi";
-import { IoNotifications } from "react-icons/io5";
+import {
+  IoNotifications,
+  IoPersonOutline,
+  IoSettingsOutline,
+} from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
 
 const Header = () => {
-  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isNotify, setIsNotify] = useState(false);
+
+  const accountDropdownRef = useRef(null);
+  const notificationDropdownRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (
+      accountDropdownRef.current &&
+      !accountDropdownRef.current.contains(event.target) &&
+      notificationDropdownRef.current &&
+      !notificationDropdownRef.current.contains(event.target)
+    ) {
+      setIsOpen(false);
+      setIsNotify(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div className="header flex justify-between px-4 pt-4 bg-[#006d77]">
-      <Image
-        src={"/images/Landmark.png"}
-        alt="logo"
-        width={150}
-        height={300}
-      ></Image>
-      <div
-        onClick={() => setIsDropdownVisible(!isDropdownVisible)}
-        className="relative cursor-pointer justify-end mr-6"
-      >
-        {/* Rounded Profile Image */}
-        <div className="flex text-white gap-4">
-          <IoLogoGithub className="mt-3 size-4" />
-          <IoNotifications className="mt-3 size-4" />
-          <FiMoon className="mt-3 size-4" />
-          <div style={{ borderRadius: "50%", overflow: "hidden" }}>
-            <Image
-              src="/images/profile-pic.png"
-              alt="Admin Profile Photo"
-              width={50}
-              height={50}
+    <div className="header flex justify-between px-4 py-2 bg-[#006d77]">
+      <Image src={"/images/Landmark.png"} alt="logo" width={150} height={150} />
+      <div className="flex text-white gap-4 justify-center items-center my-1">
+        <div className="relative">
+          <a
+            href="#"
+            class="dropdown-toggle"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <IoNotifications
+              onClick={() => {
+                setIsNotify(!isNotify);
+                setIsOpen(false);
+              }}
+              className="text-xl cursor-pointer "
             />
-          </div>
+            {isNotify && (
+              <div
+                ref={notificationDropdownRef}
+                className="absolute right-0 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50 transform translate-y-4"
+              >
+                <div className="relative py-1">
+                  <div className="px-4 py-2 text-sm text-gray-700 font-semibold">
+                    {/* <!-- Header --> */}
+                    <div class="text-center">
+                      <div className="text-white bg-[#006d77] py-1">
+                        Notifications
+                      </div>
+                    </div>
+                    {/* <!-- Notification Item --> */}
+                    <div class="flex items-center p-4 border-b">
+                      <div class="mr-4">
+                        <IoNotifications />
+                      </div>
+                      <div>
+                        <p class="font-semibold">John Doe</p>
+                        <p class="text-gray-600 text-sm">
+                          You have a new message
+                        </p>
+                      </div>
+                    </div>
+
+                    {/* <!-- Another Notification Item --> */}
+                    <div class="flex items-center p-4 border-b">
+                      <div class="mr-4">
+                        <IoIosLogOut />
+                      </div>
+                      <div>
+                        <p class="font-semibold">Jane Smith</p>
+                        <p class="text-gray-600 text-sm">
+                          Your order has been shipped
+                        </p>
+                      </div>
+                    </div>
+
+                    {/* <!-- Footer --> */}
+                    <div class="p-4 text-center">
+                      <button class="text-blue-600 hover:underline">
+                        See All Notifications
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div class="notify">
+              <span class="heartbit"></span> <span class="point"></span>{" "}
+            </div>
+          </a>
         </div>
 
-        {/* Dropdown Menu */}
-        {isDropdownVisible && (
+        <div className="relative">
           <div
-            className="rounded-xl"
-            style={{
-              position: "absolute",
-              right: 0,
-              backgroundColor: "#f9f9f9",
-              minWidth: "160px",
-              boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)",
-              zIndex: 1,
+            onClick={() => {
+              setIsOpen(!isOpen);
+              setIsNotify(false); // Ensure the notification dropdown is closed
             }}
+            className="cursor-pointer"
           >
-            <a
-              href="#"
-              className="dropdown-item"
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              <div className="flex gap-2 mx-2 my-2">
-                <p>Logout</p>
-                {/* Assuming LogoutOutlined is imported correctly */}
-              </div>
-            </a>
+            <div style={{ borderRadius: "50%", overflow: "hidden" }}>
+              <Image
+                src="/images/profile-pic.png"
+                alt="Admin Profile Photo"
+                width={40}
+                height={40}
+              />
+            </div>
           </div>
-        )}
+
+          {isOpen && (
+            <div
+              ref={accountDropdownRef}
+              className="absolute right-0 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50 transform translate-y-[20%]"
+            >
+              <div
+                className="py-1"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu"
+              >
+                <div className="px-4 py-2 text-sm text-gray-700 font-semibold">
+                  My Account
+                </div>
+                <a
+                  href="#"
+                  className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  role="menuitem"
+                >
+                  <IoPersonOutline className="mr-2" />
+                  <p>Profile</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  role="menuitem"
+                >
+                  <IoSettingsOutline className="mr-2" />
+                  <p>Settings</p>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  role="menuitem"
+                >
+                  <IoIosLogOut className="mr-2" />
+                  <p>Logout</p>
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation"; // or 'next/navigation' if using app directory
+import { useRouter } from "next/navigation"; // or 'next/router' if using pages directory
 import { GiHamburgerMenu } from "react-icons/gi";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { TbCategoryPlus } from "react-icons/tb";
@@ -10,6 +10,7 @@ import {
   IoPersonAddOutline,
   IoAnalyticsSharp,
 } from "react-icons/io5";
+import { FaArrowDown, FaArrowRight } from "react-icons/fa";
 import { MdOutlineCategory } from "react-icons/md";
 import { FaUserShield } from "react-icons/fa6";
 import { BsBoxes, BsCartCheck } from "react-icons/bs";
@@ -17,6 +18,7 @@ import { BsBoxes, BsCartCheck } from "react-icons/bs";
 const Sidebar = ({ children }) => {
   const [open, setOpen] = useState(true);
   const [isMobile, setIsMobile] = useState(false);
+  const [orderMenuOpen, setOrderMenuOpen] = useState(false); // State to toggle submenu
   const router = useRouter();
 
   const currentPath = router.pathname;
@@ -27,7 +29,7 @@ const Sidebar = ({ children }) => {
       link: "../adminPanel/dashboard",
       icon: LuLayoutDashboard,
     },
-    { name: "Users", link: "../adminPanel/signedUsers", icon: FaUserShield },
+    { name: "Users", link: "../adminPanel/users", icon: FaUserShield },
     {
       name: "Products",
       link: "../adminPanel/products",
@@ -48,7 +50,15 @@ const Sidebar = ({ children }) => {
       link: "../adminPanel/customers",
       icon: IoPersonAddOutline,
     },
-    { name: "Orders", link: "../adminPanel/orders", icon: BsCartCheck },
+    {
+      name: "Orders",
+      icon: BsCartCheck,
+      subMenu: [
+        { name: "Pending", link: "../adminPanel/orders/pending" },
+        { name: "Delivered", link: "../adminPanel/orders/delivered" },
+        { name: "Shipped", link: "../adminPanel/orders/shipped" },
+      ],
+    },
     {
       name: "Analytics",
       link: "../adminPanel/analytics",
@@ -78,6 +88,10 @@ const Sidebar = ({ children }) => {
     }
   };
 
+  const toggleOrderMenu = () => {
+    setOrderMenuOpen(!orderMenuOpen);
+  };
+
   return (
     <div className="flex">
       <div
@@ -98,15 +112,13 @@ const Sidebar = ({ children }) => {
                 open ? "flex justify-center items-center" : "font-bold"
               }`}
             />
-            <Link href="/">
-              <h2
-                className={`whitespace-pre duration-500 ${
-                  open ? "visible" : "hidden"
-                }`}
-              >
-                Admin Panel
-              </h2>
-            </Link>
+            <h2
+              className={`whitespace-pre duration-500 ${
+                open ? "visible" : "hidden"
+              }`}
+            >
+              Admin Panel
+            </h2>
           </div>
           <span className="border-b-[1px] w-full p-2 border-gray-200"></span>
           <div className="flex flex-col items-center w-full">
@@ -117,39 +129,98 @@ const Sidebar = ({ children }) => {
             >
               <div className="options space-y-1 cursor-pointer flex flex-col gap-3 relative">
                 {menus?.map((menu, i) => (
-                  <Link
-                    href={menu.link}
-                    key={i}
-                    className={`${
-                      menu?.margin && "mt-5"
-                    } group flex items-center text-sm gap-3.5 py-2 px-3 hover:bg-[#edf6f9] hover:text-black ${
-                      currentPath === menu.link && "bg-[#edf6f9]"
-                    }`}
-                  >
-                    <div className="flex-shrink-0">
-                      {React.createElement(menu.icon, {
-                        size: "23",
-                        color: "#006d77",
-                      })}
-                    </div>
-                    <h2
-                      style={{
-                        transitionDelay: `${i + 3}00ms`,
-                      }}
-                      className={`whitespace-pre duration-500 ${
-                        !open && "opacity-0 translate-x-28 overflow-hidden"
-                      }`}
-                    >
-                      {menu.name}
-                    </h2>
-                    <h2
-                      className={`${
-                        open && "hidden"
-                      } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-4 group-hover:duration-300 group-hover:w-fit z-10`}
-                    >
-                      {menu.name}
-                    </h2>
-                  </Link>
+                  <div key={i}>
+                    {menu.subMenu ? (
+                      <>
+                        <div
+                          className={`flex items-center justify-between text-sm gap-3.5 py-2 px-3 hover:bg-[#edf6f9] hover:text-black
+                          }`}
+                          onClick={toggleOrderMenu}
+                        >
+                          <div className="flex items-center gap-3.5">
+                            <div className="flex-shrink-0">
+                              {React.createElement(menu.icon, {
+                                size: "23",
+                                color: "#006d77",
+                              })}
+                            </div>
+                            <h2
+                              style={{
+                                transitionDelay: `${i + 3}00ms`,
+                              }}
+                              className={`whitespace-pre duration-500 ${
+                                !open &&
+                                "opacity-0 translate-x-28 overflow-hidden"
+                              }`}
+                            >
+                              {menu.name}
+                            </h2>
+                          </div>
+                          <div className="flex-shrink-0">
+                            {orderMenuOpen ? (
+                              <FaArrowDown
+                                size={13}
+                                className="text-gray-300"
+                              />
+                            ) : (
+                              <FaArrowRight
+                                size={13}
+                                className="text-gray-300"
+                              />
+                            )}
+                          </div>
+                        </div>
+                        {orderMenuOpen && open && (
+                          <div className="pl-8 space-y-1">
+                            {menu.subMenu.map((subMenu, j) => (
+                              <Link
+                                href={subMenu.link}
+                                key={j}
+                                className={`block text-sm py-2 px-3 hover:bg-[#edf6f9] hover:text-black ${
+                                  currentPath === subMenu.link && "bg-[#edf6f9]"
+                                }`}
+                              >
+                                {subMenu.name}
+                              </Link>
+                            ))}
+                          </div>
+                        )}
+                      </>
+                    ) : (
+                      <Link
+                        href={menu.link}
+                        className={`${
+                          menu?.margin && "mt-5"
+                        } group flex items-center text-sm gap-3.5 py-2 px-3 hover:bg-[#edf6f9] hover:text-black ${
+                          currentPath === menu.link && "bg-[#edf6f9]"
+                        }`}
+                      >
+                        <div className="flex-shrink-0">
+                          {React.createElement(menu.icon, {
+                            size: "23",
+                            color: "#006d77",
+                          })}
+                        </div>
+                        <h2
+                          style={{
+                            transitionDelay: `${i + 3}00ms`,
+                          }}
+                          className={`whitespace-pre duration-500 ${
+                            !open && "opacity-0 translate-x-28 overflow-hidden"
+                          }`}
+                        >
+                          {menu.name}
+                        </h2>
+                        {/* <h2
+                          className={`${
+                            open && "hidden"
+                          } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-4 group-hover:duration-300 group-hover:w-fit z-10`}
+                        >
+                          {menu.name}
+                        </h2> */}
+                      </Link>
+                    )}
+                  </div>
                 ))}
               </div>
             </div>
